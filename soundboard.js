9434276ece225cb5fbcb485b83e19599ac603fb4
justinbake <bakerj72@msu.edu>
2020-11-15 17:50:17 -0500

    added sounds and buttons

---
+++ soundboard.js
@@ -0,0 +1,13 @@
+
+    function playAhem(){
+        document.getElementById('ahem').play();
+    }
+
+    function playAirRaid(){
+        document.getElementById('airraid').play();
+    }
+
+    function playAlarmBeep(){
+        document.getElementById('alarmbeep').play();
+    }
+
